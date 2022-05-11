69ec97dec34ed8ab4abd943282d43d3d6fe6d980
taishi0502 <kimutai_71@icloud.com>
2022-05-11 19:12:23 +0900

    修正依頼の訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, 
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   
+
   validates :nick_name, presence: true
-  validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-
 end

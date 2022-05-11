30de57af003ad077d2e2ef77c2b1953d009c341c
taishi0502 <kimutai_71@icloud.com>
2022-05-11 17:37:07 +0900

    ユーザー管理機能テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :validatable
    
-
   validates :nick_name, presence: true
   validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :first_name, presence: true, format: { with: /\A[一-龥]+\z/}
-  validates :last_name, presence: true, format: { with: /\A[一-龥]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true

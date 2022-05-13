93b32e65b5190ee9ff8eefbb53b478476654259e
taishi0502 <kimutai_71@icloud.com>
2022-05-13 21:57:17 +0900

    修正依頼訂正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
-    
   end
 
   describe '商品の出品' do
@@ -19,63 +18,55 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-     
+
       it 'item_nameが空では投稿できない' do
         @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
-
       end
-     
+
       it 'item_descririptionが空では投稿できない' do
         @item.item_description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item description can't be blank")
-
       end
-     
+
       it 'categoryが空では投稿できない' do
-        @item.category_id = ''
+        @item.category_id = '', nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
-
       end
-     
+
       it 'conditionが空では投稿できない' do
-        @item.condition_id = ''
+        @item.condition_id = '', nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
-
       end
-     
+
       it 'delivery_feeが空では投稿できない' do
-        @item.delivery_fee_id = ''
+        @item.delivery_fee_id = '', nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
-
       end
-     
+
       it 'prefectureが空では投稿できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = '', nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-
       end
-     
+
       it 'preparation_dateが空では投稿できない' do
-        @item.preparation_date_id = ''
+        @item.preparation_date_id = '', nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Preparation date can't be blank")
-
       end
-     
+
       it 'priceが空では投稿できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
-
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
-     
+
       it '金額が¥300より下だと出品できない' do
         @item.price = 299
         @item.valid?

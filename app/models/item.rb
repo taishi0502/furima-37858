ef8689b8c58127587a09f54eeba13c31a37f5cb9
taishi0502 <kimutai_71@icloud.com>
2022-05-15 14:07:30 +0900

    各ファイルモデル名修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :deliveryfee
+  belongs_to :delivery_fee
 
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
